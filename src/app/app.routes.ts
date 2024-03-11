5dd919917e1524a06b578b506662a7aa138bd50b
harmen46 <harmenvandeb@gmail.com>
2024-03-11 13:11:44 +0100

    niks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import path from "path";
+import {HomeIndexComponent} from "./Core/Home/Home-index/home-index/home-index.component";
+import {OrderComponent} from "./Core/Order/order/order.component";
+import {BookingComponent} from "./Core/Booking/booking/booking.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home/index',
+    component: HomeIndexComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home/index',
+    pathMatch: 'full'
+  },
+  {
+    path: 'booking',
+    component: BookingComponent
+  },
+  {
+    path: 'order',
+    component: OrderComponent
+  },
+];
